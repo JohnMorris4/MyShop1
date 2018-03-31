836698a4fb57ec25abb982da7a1f776f164b6cb1
JohnMorris4 <johnmorris@morrisje.com>
2018-03-31 07:42:27 -0400

    Setup SQL Repository and did some backend work for model injection.

--- MyShop/MyShop.WebUI/App_Start/UnityConfig.cs
+++ MyShop/MyShop.WebUI/App_Start/UnityConfig.cs
@@ -2,6 +2,7 @@ using System;
 using MyShop.Core.Contracts;
 using MyShop.Core.Models;
 using MyShop.DataAccess.InMemory;
+using MyShop.DataAccess.SQL;
 using Unity;
 
 namespace MyShop.WebUI
@@ -44,8 +45,8 @@ namespace MyShop.WebUI
 
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
-            container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
-            container.RegisterType<IRepository<ProductCategory>, InMemoryRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
+            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
         }
     }
 }
