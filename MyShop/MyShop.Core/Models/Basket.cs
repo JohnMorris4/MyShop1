4806bdc67f8ccab419d86e812ddb2044d92ef963
JohnMorris4 <johnmorris@morrisje.com>
2018-04-01 06:26:44 -0400

    Initial Setup of the Basket and Basket Item Models

---
+++ MyShop/MyShop.Core/Models/Basket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+   public class Basket : BaseEntity
+    {
+        public virtual ICollection<BasketItem> BasketItems { get; set; }
+
+        public Basket()
+        {
+            this.BasketItems = new List<BasketItem>();
+        }
+     }
+}
