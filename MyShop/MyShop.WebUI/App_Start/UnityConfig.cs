331ec3e0e9bf285f4c074e590f1d62689c38b553
JohnMorris4 <johnmorris@morrisje.com>
2018-04-01 18:59:21 -0400

    stopping for the day

--- MyShop/MyShop.WebUI/App_Start/UnityConfig.cs
+++ MyShop/MyShop.WebUI/App_Start/UnityConfig.cs
@@ -3,6 +3,7 @@ using MyShop.Core.Contracts;
 using MyShop.Core.Models;
 using MyShop.DataAccess.InMemory;
 using MyShop.DataAccess.SQL;
+using MyShop.Services;
 using Unity;
 
 namespace MyShop.WebUI
@@ -47,6 +48,9 @@ namespace MyShop.WebUI
             // container.RegisterType<IProductRepository, ProductRepository>();
             container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
             container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Basket>, SQLRepository<Basket>>();
+            container.RegisterType<IRepository<BasketItem>, SQLRepository<BasketItem>>();
+            container.RegisterType<IBasketService, BasketService>();
         }
     }
 }
---
+++ MyShop/MyShop.WebUI/Controllers/BasketController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyShop.Core.Contracts;
+
+namespace MyShop.WebUI.Controllers
+{
+    public class BasketController : Controller
+    {
+        private IBasketService basketService;
+
+        public BasketController(IBasketService BasketService)
+        {
+            this.basketService = BasketService;
+
+        }
+        // GET: Basket
+        public ActionResult Index()
+        {
+            var model = basketService.GetBasketItems(this.HttpContext);
+            return View(model);
+        }
+    }
+}
